6d15c365e878225c71bd5dbf529b0784e8563bf4
Compeador <qj6w94g0q@gmail.com>
2019-11-11 14:39:08 +0800

    add no precompile asset to run on heroku

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,9 @@ module GuaneiArk
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Disable precompile in order to run on heroku
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
