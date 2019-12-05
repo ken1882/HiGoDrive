1241879ab379d6d332bf6077aadca8f276044425
Compeador <qj6w94g0q@gmail.com>
2019-12-05 16:28:31 +0800

    update basic api usage

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 CurrentVersion = 'v0'
 Rails.application.routes.draw do
-  get 'session/new'
+  # get 'session/new'
   # home page
   root 'static_pages#home'
   get '/index', to: 'static_pages#home'
   get '/home', to: 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get '/signup', to: 'static_pages#signup'
-  get '/login', to: 'session#new'
-  post '/login', to: 'session#create'
-  delete '/logout', to: 'session#destroy'
-  get '/logout', to: 'session#destroy'
+  # get '/help', to: 'static_pages#help'
+  # get '/signup', to: 'static_pages#signup'
+  # get '/login', to: 'session#new'
+  # post '/login', to: 'session#create'
+  # delete '/logout', to: 'session#destroy'
+  # get '/logout', to: 'session#destroy'
 
   match '/users' => "api/#{CurrentVersion}/users#show", via: [:get]
   match '/user'  => "api/#{CurrentVersion}/users#show", via: [:get]
