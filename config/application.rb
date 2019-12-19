2bc39c244eb11f9c8d2769e82d9463802d5ed236
ken1882 <qj6w94g0q@gmail.com>
2019-12-19 17:29:24 +0800

    sync backend code with master

--- config/application.rb
+++ config/application.rb
@@ -33,9 +33,6 @@ module GuaneiArk
 
     # Disable precompile in order to run on heroku
     config.assets.initialize_on_precompile = false
-
-    # Enforce https in production
-    config.force_ssl = true if Rails.production?
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
